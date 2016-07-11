b4d433a066e9777beb16a841b57a23c859308271
Michael Hottman <hottmanmichael@gmail.com>
2016-07-11 00:55:36 -0600

    update to admin button route and drew out basic route outline for admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,28 @@ var express = require('express'),
     router = express.Router();
 
 
+//Menu for all admin functionalities
 router.get('/', function(req, res) {
     res.render('admin/admin');
 });
+
+
+//get upload photo
+
+//post upload photo
+
+//put update photo details
+
+//delete photo
+
+//get content changes
+
+//post content changes
+
+//get update admin user settings
+    //name, email, password
+
+//post update settings
 
 
 
